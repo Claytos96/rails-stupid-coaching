cc1676bbd1cd4601e7129efdb6432c76b93c3453
Hugh Clayton <hughgrassbyclayton@hotmail.com>
2024-05-20 17:47:12 +1000

    d

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+    @foo = "adadad"
+  end
+
+  def answer
+    q = params[:question]
+    if q.ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif q == 'I am going to work'
+      @answer = 'Great!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
